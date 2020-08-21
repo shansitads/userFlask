c4dcbd0b8c14533f35a8d0c64e5bb595e5515776
shansitads <shansitads@gmail.com>
2020-08-21 19:46:22 +0530

    Fixed index redirect error

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 from flask import Flask, render_template, request, redirect, session
 from flask_session import Session
+from tempfile import mkdtemp
 from cs50 import SQL
+from helpers import login_required
+import os
+from functools import wraps
+
 
 app = Flask(__name__)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_PERMANENTON_TYPE"] = "filesystem"
-Session(app)
+app.secret_key = 'Thisissupposedtobesecret!'
+app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
+# Session(app)
 
 db = SQL("sqlite:///users.db")
 
 @app.route("/")
+@login_required
 def index():
     return render_template("index.html")
 
@@ -35,28 +44,34 @@ def register():
             return render_template("apology.html", message="Username already exists. Please choose a different username.")
 
         # If registration succeeds, add user to database
-        hashpass = hash(request.form.get("password"))
+        hashpass = request.form.get("password") # hash
         db.execute("INSERT INTO users (name, username, password) VALUES (:name, :username, :password)", name=name, username=username, password=hashpass)
         return redirect("/")
 
-@app.route("/login", methods=["GET, POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    session.clear()
     # login page is fetched
     if request.method == "GET":
         return render_template("login.html")
-
     # data is posted from login page
     else:
-        # Ensure username was submitted
-        if not request.form.get("username") or request.form.get("password"):
+        # Ensure username and password were entered
+        if not request.form.get("userid") or not request.form.get("password"):
             return render_template("apology.html", message="You must enter all fields.")
 
         # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = :username", username=request.form.get("username"))
+        rows = db.execute("SELECT * FROM users WHERE username = :username", username=request.form.get("userid"))
+        print(rows)
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+        enteredPass = request.form.get('password') # hash
+        print(enteredPass == rows[0]['password'])
+        if len(rows) != 1 or rows[0]['password'] != enteredPass:
             return render_template("apology.html", message="invalid username and/or password.")
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["username"]
 
         # Redirect user to home page
         return redirect("/")
